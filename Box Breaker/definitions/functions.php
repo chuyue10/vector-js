1687a984e9b49c4988fe618d6a6df28e2fa4ca51
Ria Chu <chuyue10@gmail.com>
2016-08-22 16:36:31 -0400

    Revert "Merge branch 'master' of https://github.com/chuyue10/vector-js"
    This reverts commit ca09c624bc4fdf16e4cde8cc5f5ed31a13bcab5a, reversing
    changes made to d7497b9ff5ea6737cade1bbe640c44c7c66a3bc3.

--- Box Breaker/definitions/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-	function getPage(){
-		//improve code, needs to detect if the page is set and which one is set. 
-		require_once("pages/homepage.php");
-	}
-?>
--- Box Breaker/pages/testlevel.php
+++
@@ -1,5 +0,0 @@
-<?php
-//assign variables from homepage.php
-	$nickname =$_POST['nickname'];
-	echo $nickname;
-?>
